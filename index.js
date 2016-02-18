6491394cc97cb749242c7afe76021143ee89f086
P1raten <juhlinus@gmail.com>
2016-02-18 14:17:07 +0100

    Stripping some unecessary code.

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// Try-Catch
-var Code = {
-	fire: function(func) {
-		try {
-			func();
-		} catch(e) {
-			window.location.href = "http://stackoverflow.com/search?q=[js]+" + e.message;
-		}
+module.exports = function(func) {
+	try {
+		func();
+	} catch (e) {
+		console.error("http://stackoverflow.com/search?q=[js]+" + e.message);
 	}
-}
-
-module.exports = Code;
+};
